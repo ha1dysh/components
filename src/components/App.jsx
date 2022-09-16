ccee1140cd96fba88dfe12a3f26a460928ab1357
Oleksandr Haidysh <OleksHaid@gmail.com>
2022-09-16 21:24:46 +0300

    profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
+import { Profile } from './Profile/Profile.jsx';
+import user from 'jsons/user.json';
+
 export const App = () => {
   return (
     <div
       style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
+        backgroundColor: '#e7ecf2',
+        padding: '20px 0',
       }}
     >
-      React homework template
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </div>
   );
 };
