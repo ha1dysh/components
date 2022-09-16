3080e49719747b3ecca1a38bd86f7365d8423105
Oleksandr Haidysh <OleksHaid@gmail.com>
2022-09-16 21:32:58 +0300

    test01

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,35 @@
+import styles from './Profile.module.css';
+
+export function Profile({
+  username,
+  tag,
+  location,
+  stats: { followers, views, likes },
+  avatar,
+}) {
+  return (
+    <div className={styles.profile}>
+      <div className={styles.description}>
+        <img src={avatar} alt="User avatar" className={styles.avatar} />
+        <p className={styles.username}>{username}</p>
+        <p className={styles.tag}>@{tag}</p>
+        <p className={styles.location}>{location}</p>
+      </div>
+
+      <ul className={styles.stats}>
+        <li>
+          <span className={styles.label}>Followers</span>
+          <span className={styles.quantity}>{followers}</span>
+        </li>
+        <li>
+          <span className={styles.label}>Views</span>
+          <span className={styles.quantity}>{views}</span>
+        </li>
+        <li>
+          <span className={styles.label}>Likes</span>
+          <span className={styles.quantity}>{likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
